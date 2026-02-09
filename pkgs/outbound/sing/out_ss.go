088134700e066943820868af7023660d7e08ec12
komoe (Monywa) <nyeinm910@gmail.com>
2026-02-09 22:35:28 +0630

    Update out_ss.go

--- pkgs/outbound/sing/out_ss.go
+++ pkgs/outbound/sing/out_ss.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/dev7dev/uri-to-json/pkgs/parser"
-	"github.com/dev7dev/uri-to-json/pkgs/utils"
+	"github.com/komoe-monywa/uri-to-json/pkgs/parser"
+	"github.com/komoe-monywa/uri-to-json/pkgs/utils"
 	"github.com/gogf/gf/v2/encoding/gjson"
 )
 
